f8305eed214309e4bd6b3f6cacb231d97e59d4ec
Robert <robert.p1991@gmail.com>
2018-09-15 16:29:35 +0200

    remove .quasar from gitignore

--- src/components/BagsList.vue
+++ src/components/BagsList.vue
@@ -6,9 +6,16 @@
           <img src="~assets/recycled-bag.svg"/>
         </div>
       </li>
-      <li>2</li>
-      <li>3</li>
-      <li>4</li>
+      <li>
+        <div class="image-wrapper">
+          <img src="~assets/recycled-bag.svg"/>
+        </div>
+      </li>
+      <li>
+        <div class="image-wrapper">
+          <img src="~assets/recycled-bag.svg"/>
+        </div>
+      </li>
     </ul>
   </div>
 </template>
@@ -24,10 +31,15 @@ ul {
   padding: 0;
 }
 li {
+  background-color: black;
   float: left;
   display: block;
   width: 5em;
   height: auto;
+  margin: 1em;
+  padding: 2em 1em;
+  border-radius: 15px;
+  box-shadow: 1px 1px 1px black;
 }
 
 </style>
